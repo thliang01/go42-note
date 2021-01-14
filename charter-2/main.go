b52228a2e1a3649fe7e8ce3d25bd6a9cf6e9966e
Thomas Liang <thliang01@gmail.com>
2021-01-14 22:16:09 +0800

    feat: New main.go

---
+++ charter-2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//const s = "Go 語言"
+
+func main() {
+
+	//for i,r:= range s{
+	//	fmt.Print("%#U : %d\n", r, i)
+	//}
+
+	var c complex128 = 1.0 + 10i
+	fmt.Printf("The value is:%v", c)
+}
